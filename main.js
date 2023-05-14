2c88c15fff8548c91e6442964ce5b9738df28158
Arhan Inam <91776437+ArhanInam@users.noreply.github.com>
2023-05-14 13:17:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
+
+Status = "";
+input_text = "";
+
 function setup()
 {
     canvas = createCanvas(480, 380);
-    canvas.center()
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(440,340);
+    video.hide();
+}
+
+function start()
+{
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    input_text = document.getElementById("input_id").value;
+}
+
+function modelLoaded()
+{
+    console.log("Model_Loaded");
+    Status = true;
 }
 
 function draw()
 {
-    image(0, 0, 480, 380);
+    image(video, 20, 20, 440, 340);
 }
+
